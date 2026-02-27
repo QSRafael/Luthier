1d3dc882d1b6014c652b1d9039c6a58e6a09e7c9
QSRafael <rfl.dqs@proton.me>
2026-02-26 21:01:54 -0300

    refactor(fe): extract state to luthier-controller-state

--- apps/luthier/src/features/luthier/useLuthierController.ts
+++ apps/luthier/src/features/luthier/useLuthierController.ts
@@ -53,6 +53,7 @@ import {
   RuntimePreference,
   RuntimePrimary
 } from '../../models/config'
+import { createLuthierState } from './luthier-controller-state'
 
 type WinetricksAvailableOutput = {
   source: string
@@ -96,39 +97,68 @@ const dedupeUrls = (values: string[]) => {
 }
 
 export function useLuthierController() {
-  const initialLocale = detectLocale()
-  const [locale, setLocale] = createSignal<Locale>(initialLocale)
-  const [activeTab, setActiveTab] = createSignal<LuthierTab>('game')
-
-  const [outputPath, setOutputPath] = createSignal('./tmp/luthier')
-  const [gameRoot, setGameRoot] = createSignal('./tmp')
-  const [gameRootManualOverride, setGameRootManualOverride] = createSignal(false)
-  const [exePath, setExePath] = createSignal('')
-  const [registryImportPath, setRegistryImportPath] = createSignal('')
-  const [iconPreviewPath, setIconPreviewPath] = createSignal('')
-  const [heroImageProcessing, setHeroImageProcessing] = createSignal(false)
-  const [heroImageAutoSearching, setHeroImageAutoSearching] = createSignal(false)
-  const [statusMessage, setStatusMessage] = createSignal(translate(initialLocale, 'statusReady'))
-  const [resultJson, setResultJson] = createSignal('')
-
-  const [winetricksAvailable, setWinetricksAvailable] = createSignal<string[]>([])
-  const [winetricksLoading, setWinetricksLoading] = createSignal(false)
-  const [winetricksSource, setWinetricksSource] = createSignal('fallback')
-  const [winetricksSearch, setWinetricksSearch] = createSignal('')
-  const [winetricksLoaded, setWinetricksLoaded] = createSignal(false)
-  const [winetricksCatalogError, setWinetricksCatalogError] = createSignal(false)
-  const [hashingExePath, setHashingExePath] = createSignal('')
-  const [lastHashedExePath, setLastHashedExePath] = createSignal('')
-  const [extractingExecutableIcon, setExtractingExecutableIcon] = createSignal(false)
-  const [testingConfiguration, setTestingConfiguration] = createSignal(false)
-  const [creatingExecutable, setCreatingExecutable] = createSignal(false)
-  const [lastPreparedHeroImageUrl, setLastPreparedHeroImageUrl] = createSignal('')
-  const [heroImageSearchCacheGameName, setHeroImageSearchCacheGameName] = createSignal('')
-  const [heroImageSearchCacheGameId, setHeroImageSearchCacheGameId] = createSignal<number | null>(null)
-  const [heroImageSearchCandidates, setHeroImageSearchCandidates] = createSignal<string[]>([])
-  const [heroImageSearchIndex, setHeroImageSearchIndex] = createSignal(0)
-
-  const [config, setConfig] = createSignal<GameConfig>(defaultGameConfig())
+  const {
+    initialLocale,
+    locale,
+    setLocale,
+    activeTab,
+    setActiveTab,
+    outputPath,
+    setOutputPath,
+    gameRoot,
+    setGameRoot,
+    gameRootManualOverride,
+    setGameRootManualOverride,
+    exePath,
+    setExePath,
+    registryImportPath,
+    setRegistryImportPath,
+    iconPreviewPath,
+    setIconPreviewPath,
+    heroImageProcessing,
+    setHeroImageProcessing,
+    heroImageAutoSearching,
+    setHeroImageAutoSearching,
+    statusMessage,
+    setStatusMessage,
+    resultJson,
+    setResultJson,
+    winetricksAvailable,
+    setWinetricksAvailable,
+    winetricksLoading,
+    setWinetricksLoading,
+    winetricksSource,
+    setWinetricksSource,
+    winetricksSearch,
+    setWinetricksSearch,
+    winetricksLoaded,
+    setWinetricksLoaded,
+    winetricksCatalogError,
+    setWinetricksCatalogError,
+    hashingExePath,
+    setHashingExePath,
+    lastHashedExePath,
+    setLastHashedExePath,
+    extractingExecutableIcon,
+    setExtractingExecutableIcon,
+    testingConfiguration,
+    setTestingConfiguration,
+    creatingExecutable,
+    setCreatingExecutable,
+    lastPreparedHeroImageUrl,
+    setLastPreparedHeroImageUrl,
+    heroImageSearchCacheGameName,
+    setHeroImageSearchCacheGameName,
+    heroImageSearchCacheGameId,
+    setHeroImageSearchCacheGameId,
+    heroImageSearchCandidates,
+    setHeroImageSearchCandidates,
+    heroImageSearchIndex,
+    setHeroImageSearchIndex,
+    config,
+    setConfig,
+    patchConfig
+  } = createLuthierState()
 
   const configPreview = createMemo(() => JSON.stringify(config(), null, 2))
   const t = (key: string) => translate(locale(), key)
@@ -667,11 +697,6 @@ export function useLuthierController() {
       heroImageSearchCandidates().length > 1
     )
   })
-
-  const patchConfig = (updater: (prev: GameConfig) => GameConfig) => {
-    setConfig((prev) => updater(prev))
-  }
-
   const clearHeroImageSearchCache = () => {
     setHeroImageSearchCacheGameName('')
     setHeroImageSearchCacheGameId(null)
